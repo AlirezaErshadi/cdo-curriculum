b417d1c5df867adf53da6e02ff78e99fe64c77e6
Tobias Kahan <tobyk100@gmail.com>
2014-01-23 15:56:08 -0800

    Make a ENV variable for grunt to build debug instead of comrpessed.

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 GIT_ROOT=`git rev-parse --show-toplevel`
 
+if [[ $1 == "debug" ]]; then
+  export target=blockly_debug.js
+  export MOOC_DEV=1
+else
+  export target=blockly_compressed.js
+fi
 (
   cd $GIT_ROOT/blockly-core
   ./deploy.sh $1
-  if [[ $1 == "debug" ]]; then
-    target=blockly_debug.js
-  else
-    target=blockly_compressed.js
-  fi
   cp $target $GIT_ROOT/blockly/lib/blockly
 )
 
