0deeb7592aad2265e2c1246162001eed23651eac
Tobias Kahan <tobyk100@gmail.com>
2014-01-30 18:17:16 -0800

    Changed name of build_scripts to scripts but forgot to update ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ fi
 
 ssh_cmd="ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-$ssh_cmd $DEPLOY_TO "cd ~/cdo-curriculum; ./build_scripts/deploy_dashboard.sh $1"
+$ssh_cmd $DEPLOY_TO "cd ~/cdo-curriculum; ./scripts/deploy_dashboard.sh $1"
